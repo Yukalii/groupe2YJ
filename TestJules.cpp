aed8f12917eadade1b74e8bd1044ce6f17c7ffbc
ReynaD1ff <weyandtjules@gmail.com>
2024-11-27 11:34:53 +0100

    implementation des classses

---
+++ TestJules.cpp
@@ -0,0 +1,1 @@
+//kouyoumbayé
--- yanis.cpp
+++ yanis.cpp
@@ -1,24 +1,52 @@
-#include <iostream>
-#include <string>
+#ifndef JEU_DE_LA_VIE_H
+#define JEU_DE_LA_VIE_H
+
 #include <vector>
+#include <string>
 
-using namespace std;
+class JeuDeLaVie {
+private:
+    Grille grille;
+    int iterations;
 
-class test
-{
 public:
-    test()
-    {
-        cout << "Constructor called" << endl;
-    }
-    ~test()
-    {
-        cout << "Destructor called" << endl;
-    }
+    void Charger(const std::string& nomFichier);
+    void simulation();
+    void AffichageConsole();
+    void AffichageGraphique();
 };
 
-int main()
-{
-    
+class Grille {
+private:
+    std::vector<Cellule> cellules;
+
+public:
+    std::vector<Cellule> getNextEtat();
+    void updateGrille();
+    int getTaille();
+};
+
+class Cellule {
+private:
+    bool estVivante;
+    int x, y;
+
+public:
+    int countVoisinsVivants();
+    void updateEtat();
+};
+
+class InterfaceGraphique {
+public:
+    void drawGrille();
+    void updateAffichage();
+    void gereEvenements();
+};
+
+int main(){
+
+
     return 0;
 }
+
+#endif // JEU_DE_LA_VIE_H
